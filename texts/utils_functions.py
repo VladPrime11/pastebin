d31efb28db8391ed3c4001684421ca5f8c2384de
AntonPrime11 <129660367+AntonPrime11@users.noreply.github.com>
2024-09-23 22:09:19 +0300

    Add .gitignore file.
    - Removing an unnecessary function.

--- texts/utils_functions.py
+++ texts/utils_functions.py
@@ -1,6 +1,5 @@
 import shortuuid
 from .models import TextBlock
-from django.core.cache import cache
 
 
 def generate_unique_hash():
@@ -8,14 +7,3 @@ def generate_unique_hash():
         hash = shortuuid.ShortUUID().random(length=6)
         if not TextBlock.objects.filter(hash=hash).exists():
             return hash
-
-
-def get_popular_text_blocks(limit=10):
-    cache_key = 'popular_text_blocks'
-    popular_blocks = cache.get(cache_key)
-
-    if popular_blocks is None:
-        popular_blocks = list(TextBlock.objects.order_by('-views')[:limit])
-        cache.set(cache_key, popular_blocks, 3000)
-
-    return popular_blocks
